48d511347154d7fc1df4127f8ededfcda56c3791
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-02 22:37:37 -0500

    Create models_forms_EditForm.php

---
+++ messages/en_US/models_forms_EditForm.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Privacy Policy' => '',
+    'Please Read and Agree to our Privacy Policy' => '',
+    'Active' => '',
+    'Title' => '',
+    'Statement' => '',
+    'Content' => '',
+    'Mark as unseen for all users' => '',
+];
