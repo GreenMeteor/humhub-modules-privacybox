66c692cc0d62fbfb195a485c5a03d97d5f287580
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-10 13:07:55 -0500

    Enh: Add Japanese translations #1

---
+++ messages/ja/models_forms_EditForm.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Privacy Policy' => '個人情報保護方針',
+    'Please Read and Agree to our Privacy Policy' => 'プライバシーポリシーを読み、同意してください',
+    'Active' => 'アクティブ',
+    'Title' => 'タイトル',
+    'Statement' => 'ステートメント',
+    'Content' => 'コンテンツ',
+    'Mark as unseen for all users' => 'すべてのユーザーにとって見えないものとしてマークする',
+];
