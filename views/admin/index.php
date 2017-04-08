8131c6d0045f928eac257a3e16dc96283a123f96
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-08 14:03:43 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -3,40 +3,40 @@
 use Yii;
 use yii\helpers\Url;
 use yii\helpers\Html;
-use humhub\compat\ActiveForm;
+use yii\bootstrap\ActiveForm;
 
 ?>
 \
 <div class="panel panel-default">
-    <div class="panel-heading"><?php echo Yii::t('PrivacyboxModule.views_admin_index', 'Privacy Box Configuration'); ?></div>
+    <div class="panel-heading"><?= Yii::t('PrivacyboxModule.views_admin_index', 'Privacy Box Configuration'); ?></div>
     <div class="panel-body">
 
         <?php $form = ActiveForm::begin(['id' => 'privacybox-settings-form']); ?>
 
-        <?php echo $form->errorSummary($model); ?>
+        <?= $form->errorSummary($model); ?>
 
-        <?php echo $form->field($model, 'active')->checkbox(); ?>
-        <?php echo $form->field($model, 'title', ['inputOptions' =>['class' => 'form-control']]); ?>
-        <?php echo $form->field($model, 'statement', ['inputOptions' =>['class' => 'form-control']]); ?>
+        <?= $form->field($model, 'active')->checkbox(); ?>
+        <?= $form->field($model, 'title', ['inputOptions' =>['class' => 'form-control']]); ?>
+        <?= $form->field($model, 'statement', ['inputOptions' =>['class' => 'form-control']]); ?>
 
         <div class="form-group">
-             <?php echo $form->field($model, 'content', ['inputOptions' => ['class' => 'form-control', 'id' => 'newMessageText']])->textarea(); ?>
-             <?php echo \humhub\widgets\MarkdownEditor::widget(['fieldId' => 'newMessageText']); ?>
-             <?php echo $form->error($model, 'content'); ?>
+             <?= $form->field($model, 'content', ['inputOptions' => ['class' => 'form-control', 'id' => 'newMessageText']])->textarea(); ?>
+             <?= \humhub\widgets\MarkdownEditor::widget(['fieldId' => 'newMessageText']); ?>
+             <?= $form->error($model, 'content'); ?>
         </div>
 
         <div class="form-group">
             <div class="checkbox">
                 <label>
-                    <?php echo $form->checkBox($model, 'reset'); ?> <?php echo $model->getAttributeLabel('reset'); ?>
+                    <?= $form->checkBox($model, 'reset'); ?> <?= $model->getAttributeLabel('reset'); ?>
                 </label>
             </div>
         </div>
 
         <hr>
 
-        <?php echo Html::submitButton(Yii::t('PrivacyboxModule.views_admin_index', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
-        <a class="btn btn-default" href="<?php echo Url::to(['/admin/module']); ?>"><?php echo Yii::t('PrivacyboxModule.views_admin_index', 'Back to modules'); ?></a>
+        <?= Html::submitButton(Yii::t('PrivacyboxModule.views_admin_index', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+        <a class="btn btn-default" href="<?= Url::to(['/admin/module']); ?>"><?= Yii::t('PrivacyboxModule.views_admin_index', 'Back to modules'); ?></a>
 
         <?php ActiveForm::end(); ?>
     </div>
